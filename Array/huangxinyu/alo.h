9ac3e46f8c861b737eb6845e70505458531729e0
ryujin <1250242270@qq.com>
2020-06-02 07:44:42 +0800

    climb stairs

---
+++ Array/huangxinyu/alo.h
@@ -0,0 +1,11 @@
+//
+// Created by hasee on 2020/6/2.
+//
+
+#ifndef LEETCODE_ALO_H
+#define LEETCODE_ALO_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+int climbStairs(int n);
+#endif //LEETCODE_ALO_H
