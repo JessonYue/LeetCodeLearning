c62fe0c1198bf3d953ac9d1b4933145cbcbe86dd
majy@9stars.cn <1505508999@qq.com>
2020-06-01 13:36:34 +0800

    两数之和c/c++两种实现方式

---
+++ Array/majiyao/两数之和c:c++两种实现方式/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void sumArray(int* array, int target);
+
+//1. 两数之和
+int main() {
+    printf("Hello, World!\n");
+    int array[] = {2, 7, 11, 15};
+    int target = 9;
+    sumArray(array, target);
+    return 0;
+}
+
+void sumArray01(int* array, int target) {
+    //获取数组的长度
+    int size = sizeof(array) / sizeof(int);
+    for (int i = 0; i < size; ++i) {
+        //取值
+        int a = array[i];
+        for (int j = 0; j < size; ++j) {
+            int b = array[j];
+            if (a != b && a + b == target && i < j){
+                printf("index value is %d, %d\n", i+1, j+1);
+                break;
+            }
+        }
+    }
+}
+
+void sumArray(int* array, int target){
+    int size = sizeof(array) / sizeof(int);
+    set<>
+    for (int i = 0; i < size; ++i) {
+        int a = target - array[i];
+        if()
+    }
+
+}
+
