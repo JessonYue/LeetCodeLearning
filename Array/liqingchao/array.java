9c1a93387b66d9391d26f720651bce4e2db342b9
liqingchao <liqingchao@aladdin-holdings.com>
2020-06-03 22:04:14 +0800

    6.3号算法

--- Array/liqingchao/array.java
+++ Array/liqingchao/array.java
@@ -82,3 +82,50 @@
         }
         return third;
     }
+
+     /**
+     * 2020.06.3
+     * 28. 实现 strStr() //https://leetcode-cn.com/problems/implement-strstr/
+     * 实现 strStr() 函数。
+     * <p>
+     * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: haystack = "hello", needle = "ll"
+     * 输出: 2
+     * 示例 2:
+     * <p>
+     * 输入: haystack = "aaaaa", needle = "bba"
+     * 输出: -1
+     * 说明:
+     * <p>
+     * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+     * <p>
+     * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+     */
+    public int strStr(String haystack, String needle) {
+        if (haystack.length() == 0 || needle.length() == 0) {
+            return -1;
+        }
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+        char[] maxChars = haystack.toCharArray();
+        char[] minChars = needle.toCharArray();
+        int maxLen = maxChars.length - minChars.length;
+        for (int i = 0; i <= maxLen; i++) {
+            if (maxChars[i] != minChars[0]) {
+                while (++i <= maxLen && maxChars[i] != minChars[0]);
+            }
+            if (i <= maxLen) {
+                int j = i + 1;
+                int end = j + needle.length() - 1;
+                for (int k = 1; j < end && minChars[k] == maxChars[j]; k++, j++) ;
+                if (j == end) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
