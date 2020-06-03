bcb0828708c3270358e6e8275727a21ef8b434c8
linchuanxin2007@163.com <linchuanxin2007@163.com>
2020-06-03 17:21:09 +0800

    day02算法增加动态规划解法(c和java)

--- Array/linchuanxin/Algorithm/day02_爬楼梯/day02.java
+++ Array/linchuanxin/Algorithm/day02_爬楼梯/day02.java
@@ -38,5 +38,28 @@ public class day02 {
         }
         return temp;
     }
+	
+	//6.3增加动态规划解法
+	/*
+    *
+    * 动态规划
+    *
+    * 最优子结构   n-1  n-2
+    * 边界   f1=1   f2=2
+    * 状态转移方程   n-1   +   n-2
+    * */
+    private static int climb3(int n){
+        if(n==1 || n==2){
+            return n;
+        }
+        int[] arr = new int[n+1];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+
+    }
 }
 
