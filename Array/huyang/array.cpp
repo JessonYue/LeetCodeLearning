27ed7e89bb5b7d0230b455eb5ae623b00b31db02
huyang <huyang67@jd.com>
2020-06-03 22:48:44 +0800

    第三道strstr()算法C++实现，用的双遍历，kmp算法不记得了，应该效率会更好

--- Array/huyang/array.cpp
+++ Array/huyang/array.cpp
@@ -50,3 +50,35 @@ public:
         return dp[n] ;
         }
 };
+
+
+//第三道strstr()
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        if(haystack.length() < needle.length()){
+            return -1;
+        }
+
+        if(needle.length() == 0){
+            return 0;
+        }
+
+        for(int i = 0 ; i<= haystack.length() - needle.length(); i ++ ){
+            int j = 0;
+            for( j = 0 ; j < needle.length(); j ++ ){
+                if(haystack[i + j ] != needle[j]){
+                    break;
+                }
+               
+            }
+             if(j == needle.length()){
+                   return i; 
+                }
+        }
+        return -1;
+    }
+};
+
+
