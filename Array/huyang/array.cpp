e50967578388ff68f754fad37dc66cbf6b8e8dbb
huyang <huyang_fox@126.com>
2020-06-05 23:54:16 +0800

    第五道  链表是否相交

--- Array/huyang/array.cpp
+++ Array/huyang/array.cpp
@@ -124,3 +124,46 @@ public:
 
     }
 };
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* h1 = headA ,  *h2 = headB;
+
+        while(h1 || h2){
+
+            if(h1 == h2 ) {
+                return h1;
+            }
+            if(h1 == NULL){
+                h1 = headB;
+            }else{
+                h1= h1->next;
+            }
+
+            if(h2 == NULL){
+                h2 = headA;
+            }else{
+                h2 = h2->next;
+            }
+
+           
+        }
+            return NULL;
+            
+        }
+        
+
+
+    
+};
