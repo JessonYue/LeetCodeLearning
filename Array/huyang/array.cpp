fd1e7dd3dea9efa2e9103720f7c4b0cc239b7ff0
huyang <huyang_fox@126.com>
2020-06-09 14:47:22 +0800

    算法，合并排序链接

--- Array/huyang/array.cpp
+++ Array/huyang/array.cpp
@@ -196,3 +196,64 @@ public:
         return p;
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1){
+            return l2;
+        }
+        if(!l2){
+            return l1;
+        }
+               
+        ListNode* res = NULL;
+        if(l1->val >= l2->val){
+            res = l2;
+            l2 = l2->next;
+        }else{
+            res = l1;
+            l1 = l1->next;
+        }
+        ListNode* r = res;
+
+        while(l2||l1){
+            if(!l1){
+                res->next = l2;
+                l2 = l2->next;
+                res  = res->next;
+                continue;
+            }
+            if(!l2){
+                res->next = l1;
+                l1 = l1->next;
+                res  = res->next;
+                continue;
+            }
+
+
+            if((l1->val >= l2->val)){
+                res->next = l2;
+                l2 = l2->next;
+            }else{
+                res->next = l1;
+                l1 = l1->next;
+            }
+            res  = res->next;
+        }
+        return r;
+
+        
+    }
+};
