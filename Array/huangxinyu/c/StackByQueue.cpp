96c8eb1813cdae43634f39f6d270900cbdf997b3
ryujin <1250242270@qq.com>
2020-06-15 07:42:55 +0800

    使用队列实现栈c++

---
+++ Array/huangxinyu/c/StackByQueue.cpp
@@ -0,0 +1,31 @@
+//
+// Created by hasee on 2020/6/15.
+//
+
+#include "StackByQueue.h"
+
+/**
+ * 每次新元素先入队，然后把旧元素取出重新入队，时间复杂度O(n)，空间O(1)
+ * 1. push 1    [1]
+ * 2. push 2    [1,2],[2,1]
+ * 3. push 3    [2,1,3],[3,2,1]
+ */
+void StackByQueue::push(int x) {
+    queue.push(x);
+    while (queue.size() > 1) {
+        queue.push(queue.front());
+        queue.pop();
+    }
+}
+
+void StackByQueue::pop() {
+    queue.pop();
+}
+
+int StackByQueue::top() {
+    return queue.front();
+}
+
+int StackByQueue::empty() {
+    return queue.empty();
+}
