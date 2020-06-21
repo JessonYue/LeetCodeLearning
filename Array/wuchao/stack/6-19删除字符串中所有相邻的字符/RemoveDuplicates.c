8c43385fa6cb75ec96a16ff49331e4e6dbf58b98
wuchao <329187218@qq.com>
2020-06-21 12:53:24 +0800

    6-21 提供一种简便解法

--- Array/wuchao/stack/6-19删除字符串中所有相邻的字符/RemoveDuplicates.c
+++ Array/wuchao/stack/6-19删除字符串中所有相邻的字符/RemoveDuplicates.c
@@ -87,3 +87,28 @@ char * removeDuplicates(char * S){
     }
     return S;
 }
+
+//简便解法，利用栈的思想，将字符串当成一个栈来使用，top记录栈顶元素，writeIndex记录栈顶元素在数组的下标(覆盖的字符串的哪个位置)
+//依次遍历字符串每个字符，如果和栈顶元素相等，则writeIndex减1，则代表栈顶元素出栈了，如果栈为空即writeIndex为0或者，栈顶元素和遍历的元素不相等
+//则入栈，即将字符数组下标writeIndex的元素复制为遍历的元素。直到遍历完整个字符数组。将栈顶后面的一个元素赋值为0，代表字符串结束。返回字符串即可。
+char * removeDuplicates2(char * S){
+    unsigned long stringLength = strlen(S);
+    unsigned long i=0;
+    char top = 0;
+    int writeIndex = 0;
+    while(i<stringLength){
+        if(writeIndex==0){
+            top = 0;
+        } else {
+            top = S[writeIndex-1];
+        }
+        if(top==S[i]){
+            writeIndex--;
+        }else{
+            S[writeIndex++] = S[i];
+        }
+        i++;
+    }
+    S[writeIndex] = 0;
+    return S;
+}
