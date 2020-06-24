9cb0c940c4a0d61a480ad7130e80b2f3cc1df0c9
fanwuye <fanbei.999@163.com>
2020-06-25 00:48:59 +0800

    Array/fanbei/

---
+++ Array/fanbei/0616/c/soloution06_16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+bool isValid(char *s);
+
+bool isValid(char *s) {
+    if (s == NULL || s[0] == '\0') {
+        return true;
+    }
+    int len = strlen(s);
+    int index = -1;
+    char stack[len];
+    for (int i = 0; i < len; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            stack[++index] = s[i];
+        } else if (index == -1) {
+            return false;
+        } else if ((stack[index] + 1) == s[i] || (stack[index] + 2) == s[i]) {
+            stack[index--] = 0;
+        } else {
+            stack[++index] = s[i];
+        }
+    }
+    return index == -1;
+}
