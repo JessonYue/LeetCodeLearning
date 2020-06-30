665d189640e1ab87cabe7460c8cd0535bd44f8ca
Vashon <wxcai_sysuxh@163.com>
2020-06-30 15:18:57 +0800

    完成这两天的算法与矩阵的四则运算

--- Array/caiweixiong/c/homework_006.c
+++ Array/caiweixiong/c/homework_006.c
@@ -11,20 +11,21 @@ void homework_006_206(void) {
     void listInsert(struct ListNode **list, int e);
     void displayList(struct ListNode *list);
     void destroyList(struct ListNode **list);
-    struct ListNode *reverseList(struct ListNode *head);
+    struct ListNode *reverseList_(struct ListNode *head);
     listInsert(&list, 0);
     listInsert(&list, 9);
     listInsert(&list, 1);
     listInsert(&list, 2);
     listInsert(&list, 4);
-    printf("链表反转前的元素为：");
+    printf("链表反转前的元素为 : ");
     displayList(list);
-    list = reverseList(list);
-    printf("链表反转后的元素为：");
+    list = reverseList_(list);
+    printf("链表反转后的元素为 : ");
     displayList(list);
     destroyList(&list);
 }
 
+// 方法一：迭代反转链表
 struct ListNode *reverseList(struct ListNode *head) {
     if (head == NULL)
         return NULL;
@@ -38,4 +39,19 @@ struct ListNode *reverseList(struct ListNode *head) {
         next = temp;
     }
     return head;
+}
+
+// 方法二：递归反转链表
+struct ListNode *reverseList_(struct ListNode *head) {  // 1 -> 2 -> 3 -> 4 -> 5 -> null
+    // 递归出口
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL) // 5 -> null
+        return head;
+    // 递归
+    struct ListNode *newHead = reverseList_(head->next);// 2 -> 3 -> 4 -> 5
+    // 5 -> 4 -> null
+    head->next->next = head;
+    head->next = NULL;
+    return newHead;
 }
