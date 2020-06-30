59ec26c5e9e2f09e934e3544179ea94415d8ad2a
ryujin <1250242270@qq.com>
2020-06-30 22:13:51 +0800

    递归方式实现链表反转

--- Array/huangxinyu/c/alo.h
+++ Array/huangxinyu/c/alo.h
@@ -24,6 +24,7 @@ void merge(int *A, int ASize, int m, int *B, int BSize, int n);
 ListNode *getIntersectionNode(ListNode *headA, ListNode *headB);
 
 ListNode *reverseList(ListNode *head, int i);
+ListNode *reverseList1(ListNode *head, int i);
 
 ListNode *mergeTwoLists(ListNode *l1, ListNode *l2);
 
