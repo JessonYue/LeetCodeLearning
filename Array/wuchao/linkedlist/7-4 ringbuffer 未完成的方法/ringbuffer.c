4e45745810488f8d9cab6b8b23b69e86a4b39c4f
wuchao <329187218@qq.com>
2020-07-04 19:01:38 +0800

    7-4 ringbuffer未完成方法，问题修复，测试

---
+++ Array/wuchao/linkedlist/7-4 ringbuffer 未完成的方法/ringbuffer.c
@@ -0,0 +1,127 @@
+//
+// Created by Jesson on 2020/6/14.
+//
+
+#include <stdio.h>
+#include <assert.h>
+#define MAX_SIZE 10
+typedef int DATA_TYPE ;
+
+typedef struct Queue{
+    DATA_TYPE Queue[MAX_SIZE];
+    int front; //对头指针
+    int rear; //队尾指针
+}SeqCirQueue;
+
+
+void initQueue(SeqCirQueue* seqQueue);
+int isEmpty(SeqCirQueue* seqQueue);
+int isFull(SeqCirQueue* seqQueue);
+void enterQueue(SeqCirQueue* seqQueue,int data);
+int deleteQueue(SeqCirQueue* seqQueue,int* data); //队列删除元素，并返回这个元素
+void clean(SeqCirQueue* seqQueue);
+void printQueue(SeqCirQueue*  seqQueue);
+
+
+
+void printQueue(SeqCirQueue* seqQueue);
+void testQueue();
+
+void initQueue(SeqCirQueue* seqQueue){
+    seqQueue->front = seqQueue->rear = 0;
+}
+
+int isEmpty(SeqCirQueue* seqQueue){
+    if(seqQueue->front == seqQueue->rear){
+        return 1;
+    }
+    return 0;
+}
+
+int isFull(SeqCirQueue* seqQueue){
+    if((seqQueue->rear+1) % MAX_SIZE  == seqQueue->front){
+        return 1;
+    }
+    return 0;
+}
+
+
+void enterQueue(SeqCirQueue* seqQueue,DATA_TYPE data){
+    //1.判断是否队列是否满了
+    if(isFull(seqQueue)){
+        printf("queue is Full!!\n");
+        return ;
+    }
+    //保存起来
+    seqQueue->Queue[seqQueue->rear] = data;
+    seqQueue->rear = (seqQueue->rear +1)% MAX_SIZE;
+}
+
+int deleteQueue(SeqCirQueue* seqQueue,DATA_TYPE* data){
+    if(isEmpty(seqQueue)){
+        printf("queue is empty!!\n");
+        return -1;
+    }
+    *data =  seqQueue->Queue[seqQueue->front];
+    seqQueue->front =(seqQueue->front)+1;
+    return *data;
+}
+
+/*循环队列*/
+void printQueue(SeqCirQueue*  seqQueue){
+    assert(seqQueue);
+    if(isEmpty(seqQueue)){
+        printf("Queue is Empty");
+        return;
+    }
+    int i = seqQueue->front;
+    if(seqQueue->front < seqQueue->rear){
+        for (;i<seqQueue->rear;i++){
+            printf("%5d",seqQueue->Queue[i]);
+        }
+    }else {
+        //出队很快
+        for (;i<seqQueue->rear + MAX_SIZE;i++){
+            int index = i%MAX_SIZE;
+            printf("%5d",seqQueue->Queue[index]);
+        }
+    }
+    printf("\n");
+}
+
+void clean(SeqCirQueue* seqQueue){
+    while(!isEmpty(seqQueue)){
+        seqQueue->Queue[seqQueue->front] = -1;
+        seqQueue->front = (seqQueue->front) + 1;
+    }
+}
+
+
+void testQueue(){
+    SeqCirQueue  queue;
+    DATA_TYPE data;
+    initQueue(&queue);
+    enterQueue(&queue,1);
+    enterQueue(&queue,2);
+    enterQueue(&queue,3);
+    enterQueue(&queue,4);
+    enterQueue(&queue,5);
+    enterQueue(&queue,6);
+    enterQueue(&queue,7);
+    printQueue(&queue);
+    deleteQueue(&queue,&data);
+    deleteQueue(&queue,&data);
+    enterQueue(&queue,8);
+    enterQueue(&queue,9);
+    enterQueue(&queue,19);
+    enterQueue(&queue,119);
+   enterQueue(&queue,120);
+    printQueue(&queue);
+    clean(&queue);
+    printQueue(&queue);
+}
+
+int main(){
+    testQueue();
+
+}
