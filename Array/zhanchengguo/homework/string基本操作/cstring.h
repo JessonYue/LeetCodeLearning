376eb5f2cbf3eb80760c38ea10b08b00c595a248
zhanchengguo <956492422@qq.com>
2020-07-18 00:46:15 +0800

    字符串C语言实现

---
+++ Array/zhanchengguo/homework/string基本操作/cstring.h
@@ -0,0 +1,15 @@
+
+#ifndef UNTITLED2_CSTRING_H
+#define UNTITLED2_CSTRING_H
+
+char *my_strcpy(char *dest, char *src);
+
+char *my_strcat(char *src1, char *src2);
+
+char *my_strstr(char *src, char *dest);
+
+char *my_strchr(char *src, char dest);
+
+void *my_memcpy(char *dest, char *src,size_t n);
+
+#endif //UNTITLED2_CSTRING_H
