2bf43393fd2dbff3ca1edd8f6f6c5ebe8662a798
zhuqiguang <qiguang.zhu@foxmail.com>
2020-07-23 17:06:32 +0800

    添加283. 移动零

---
+++ Array/zhuqiguang/java/Solution_283.java
@@ -0,0 +1,13 @@
+public class Solution_283 {
+    public void moveZeroes(int[] nums) {
+        int j=0;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for(int i=j;i<nums.length;i++) {
+            nums[i]=0;
+        }
+    }
+}
